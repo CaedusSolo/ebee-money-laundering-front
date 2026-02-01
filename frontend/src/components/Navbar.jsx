d06f1c10a370d8700022993286c7a00570beb175
Ying Tong <yingtong0106@gmail.com>
2026-02-01 21:10:09 +0800

    feat: Add logo in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import navbarLogo from "../assets/navbarLogo.svg"
+
 export default function Navbar() {
   return (
       <nav className="bg-gradient-to-r from-blue-800 to-cyan-500 shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
           <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center font-bold text-blue-700">FYJ</div>
+            <div className="w-8 h-8 rounded-full flex items-center justify-center font-bold text-blue-700">
+            <img src={navbarLogo} />
+          </div>
             <h1 className="text-2xl font-bold text-white">Further Your Journey</h1>
           </div>
           <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center font-bold text-gray-600">
