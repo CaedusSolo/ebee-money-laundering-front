7d1482bd6d3f394d64cb6c6b08192a89dadb24c1
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-02 22:08:17 +0800

    feat: Add UserService

---
+++ frontend/src/services/UserService.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+class UserService {
+  constructor(jwtToken, baseUrl = "/api/users") {
+    this.api = axios.create({
+      baseURL: baseUrl,
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  async getAllUsers() {
+    const response = await this.api.get("/");
+    return response.data;
+  }
+
+  async createUser(userData) {
+    const response = await this.api.post("/", userData);
+    return response.data;
+  }
+
+  async updateUser(userId, userData) {
+    const response = await this.api.put(`/${userId}`, userData);
+    return response.data;
+  }
+
+  async deleteUser(userId) {
+    const response = await this.api.delete(`/${userId}`);
+    return response.data;
+  }
+}
+
+export default UserService;
