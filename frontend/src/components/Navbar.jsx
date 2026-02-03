dcba00d688e7f12c21748ed3b8f4336a41bdc60a
Mimi <aimimf20@gmail.com>
2026-02-04 00:20:42 +0800

    fix: merge issues fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import navbarLogo from "../assets/navbarLogo.svg"
 import { useAuth } from "../context/AuthContext"
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Navbar({ showBrowse = false }) {
   const { logout } = useAuth();
